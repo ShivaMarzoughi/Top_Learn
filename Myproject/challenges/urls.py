8bc5e5a5748b452869a069d95a8c2efdd0f2f6ee
shiva <shivamarzoughi@gmail.com>
2024-11-16 11:26:04 +0330

    تغییرات جلسه ۹

--- Myproject/challenges/urls.py
+++ Myproject/challenges/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import saturday,sunday
+from .views import saturday,sunday,dynamic_days
 
 
 urlpatterns = [
     path('saturday/',saturday),
-    path('sunday/',sunday)
+    path('sunday/',sunday),
+    # این اسم دی داینامیک هست
+    path('<day>/',dynamic_days),
+    # میشه گسترده ترش هم کرد
+    path('<day>/<description>/', dynamic_days)
+
+
 ]
--- Myproject/challenges/views.py
+++ Myproject/challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseNotFound
 
 
 
@@ -9,3 +9,27 @@ def saturday(request):
 
 def sunday(request):
     return HttpResponse('this is sunday')
+
+
+def monday(request):
+    return HttpResponse('this is monday')
+
+
+# دینامیک روت میخواییم بسازیم
+# def dynamic_days(request,day,description):
+#     return HttpResponse(f"Day: {day}, Description: {description}")
+
+
+# گسترش میدیم فرضا این دیتابیس ما هست
+days={
+    'saturday':'this is saturday',
+    'sunday':'this is sunday',
+
+}
+def dynamic_days(request,day,description):
+    # day ما اینجا همون کلید دیتابیس ما هس
+    day_data=days.get(day)
+    if day_data is not None:
+        return HttpResponse(f"Day: {day}, Description: {day_data}")
+    else:
+        return HttpResponseNotFound('day does not existe')

b43c80ce3042daad7715e84c6e466de7c2d58abb
shiva <shivamarzoughi@gmail.com>
2024-11-27 20:44:30 +0330

    تغییرات جلسه 10

--- Myproject/challenges/urls.py
+++ Myproject/challenges/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
-from .views import saturday,sunday,dynamic_days
+from .views import saturday,sunday,dynamic_days,dynamic_days_by_number
 
-
+# ترتیب قرار گرفتن این لینک ها مهم
 urlpatterns = [
     path('saturday/',saturday),
     path('sunday/',sunday),
     # این اسم دی داینامیک هست
-    path('<day>/',dynamic_days),
+    # path('<day>/',dynamic_days),
     # میشه گسترده ترش هم کرد
-    path('<day>/<description>/', dynamic_days)
-
+    # path('<day>/<description>/', dynamic_days),
+    # درست تر مینویسیم
+    path('<int:day>/',dynamic_days_by_number),
+    path('<str:day>/', dynamic_days)
 
 ]
--- Myproject/challenges/views.py
+++ Myproject/challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseNotFound
+from django.http import HttpResponse,HttpResponseNotFound,HttpResponseRedirect
 
 
 
@@ -26,10 +26,31 @@ days={
     'sunday':'this is sunday',
 
 }
-def dynamic_days(request,day,description):
+
+def dynamic_days_by_number(request,day):
+    if day>len(days.keys):
+        return HttpResponseNotFound('day does not existe')
+    # days_name=days.get(day)
+    days_names=list(days.keys())
+    redirect_day=days_names[day -1]
+    return HttpResponseRedirect(f'/days/{redirect_day}')
+    # return HttpResponse(day)  
+
+
+
+# def dynamic_days(request,day,description):
+#     # day ما اینجا همون کلید دیتابیس ما هس
+#     day_data=days.get(day)
+#     if day_data is not None:
+#         return HttpResponse(f"Day: {day}, Description: {day_data}")
+#     else:
+#         return HttpResponseNotFound('day does not existe')
+    
+def dynamic_days(request,day):
     # day ما اینجا همون کلید دیتابیس ما هس
     day_data=days.get(day)
     if day_data is not None:
         return HttpResponse(f"Day: {day}, Description: {day_data}")
     else:
         return HttpResponseNotFound('day does not existe')
+

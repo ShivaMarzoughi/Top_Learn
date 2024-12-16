c9169d28f77b6c4cabdc3d05886f751472271bd3
shiva <shivamarzoughi@gmail.com>
2024-12-16 19:18:03 +0330

    14

--- Myproject/challenges/urls.py
+++ Myproject/challenges/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
-from .views import saturday,sunday,dynamic_days,dynamic_days_by_number,days_list
-
+# from .views import saturday,sunday,dynamic_days,dynamic_days_by_number,days_list
+from .views import dynamic_days
 # ترتیب قرار گرفتن این لینک ها مهم
 urlpatterns = [
-    path('saturday/',saturday),
-    path('sunday/',sunday),
+    # path('saturday/',saturday),
+    # path('sunday/',sunday),
     # این اسم دی داینامیک هست
     # path('<day>/',dynamic_days),
     # میشه گسترده ترش هم کرد
     # path('<day>/<description>/', dynamic_days),
     # درست تر مینویسیم
-    path('<int:day>/',dynamic_days_by_number),
+    # path('<int:day>/',dynamic_days_by_number),
     path('<str:day>/', dynamic_days,name='days-of-weeks'),
-    path('',days_list)
+    # path('',days_list)
 
 ]
--- Myproject/challenges/views.py
+++ Myproject/challenges/views.py
@@ -3,16 +3,16 @@ from django.http import HttpResponse,HttpResponseNotFound,HttpResponseRedirect
 from django.urls import reverse
 from django.template.loader import render_to_string
 
-def saturday(request):
-    return HttpResponse('this is satueday')
+# def saturday(request):
+#     return HttpResponse('this is satueday')
 
 
-def sunday(request):
-    return HttpResponse('this is sunday')
+# def sunday(request):
+#     return HttpResponse('this is sunday')
 
 
-def monday(request):
-    return HttpResponse('this is monday')
+# def monday(request):
+#     return HttpResponse('this is monday')
 
 
 # دینامیک روت میخواییم بسازیم
@@ -27,17 +27,17 @@ days={
 
 }
 
-def dynamic_days_by_number(request,day):
-    if day>len(days.keys):
-        return HttpResponseNotFound('day does not existe')
-    # days_name=days.get(day)
-    days_names=list(days.keys())
-    redirect_day=days_names[day -1]
-    redirect_url= reverse('days-of-week',args=[redirect_day])  #/days/
-    return HttpResponseRedirect(redirect_url)
+# def dynamic_days_by_number(request,day):
+#     if day>len(days.keys):
+#         return HttpResponseNotFound('day does not existe')
+#     # days_name=days.get(day)
+#     days_names=list(days.keys())
+#     redirect_day=days_names[day -1]
+#     redirect_url= reverse('days-of-week',args=[redirect_day])  #/days/
+#     return HttpResponseRedirect(redirect_url)
 
-    # return HttpResponseRedirect(f'/days/{redirect_day}')
-    # return HttpResponse(day)  
+#     # return HttpResponseRedirect(f'/days/{redirect_day}')
+#     # return HttpResponse(day)  
 
 
 
@@ -54,26 +54,30 @@ def dynamic_days(request,day):
     day_data=days.get(day)
     if day_data is not None:
         # response_data=f'<h1> Day: {day}, Description: {day_data} </h1>'
-        response_data=render_to_string('challenges/challenge.html')
-        return HttpResponse(response_data)
+        # response_data=render_to_string('challenges/challenge.html')
+        # return HttpResponse(response_data)
+        context={
+            'data':day_data
+        }
+        return render(request,'challenges/challeng.html',context)
     else:
         return HttpResponseNotFound('day does not existe')
 
 
-def days_list(request):
-    # به صورت داینامیک میسازیم
-    days_list=list(days.keys())
-    list_item=''
-    for day in days_list:
-        url_path=reverse('days-of-week',args=[day])
-        list_item += f'<li> <a href="{url_path}">{day}</a> </li>'
+# def days_list(request):
+#     # به صورت داینامیک میسازیم
+#     days_list=list(days.keys())
+#     list_item=''
+#     for day in days_list:
+#         url_path=reverse('days-of-week',args=[day])
+#         list_item += f'<li> <a href="{url_path}">{day}</a> </li>'
 
-    content =f'<ul> {list_item} </ul>'
+#     content =f'<ul> {list_item} </ul>'
 
-    # content ='''
-    #     <ul> 
-    #      <li> saturday </li>
-    #     </ul>
+#     # content ='''
+#     #     <ul> 
+#     #      <li> saturday </li>
+#     #     </ul>
 
-    # '''
-    return HttpResponse(content)
+#     # '''
+#     return HttpResponse(content)
